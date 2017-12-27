2d431be1af8e9bcc593d6a95fab7d5e110635756
GRIM <694130310@qq.com>
2017-12-27 16:10:49 +0800

    测试视频控件

--- Hachi/App/Video/VideoDetail.js
+++ Hachi/App/Video/VideoDetail.js
@@ -10,10 +10,12 @@ import {
     TextInput,
     Image,
     Platform,
-    ScrollView
+    ScrollView,
+    Dimensions
 } from 'react-native';
 
 var Video = require('react-native-video').default
+var {width, height} = Dimensions.get('window');
 
 var VideoDetail = React.createClass({
 
@@ -21,18 +23,18 @@ var VideoDetail = React.createClass({
         return{
             data: this.props.rowData,
             rate: 1,
-            muted: true,
+            muted: false,
             resizeMode: 'contain',
             repeat: false
         }
     },
 
     render() {
-        var rowData = this.props.rowData
-        console.log(rowData)
+        var data = this.props.rowData
+        console.log(data)
         return (
             <View style={styles.container}>
-                <Text>视频详情{rowData._id}</Text>
+                <Text>视频详情{data._id}</Text>
                 <View style={styles.videoBox}>
                     <Video
                         ref='videoPlayer'
@@ -50,7 +52,6 @@ var VideoDetail = React.createClass({
                         onProgress={this._onProgress}
                         onEnd={this._onEnd}
                         onError={this._onError}
-
                     >
                     </Video>
                 </View>
@@ -60,23 +61,27 @@ var VideoDetail = React.createClass({
 
     //视频刚开始加载瞬间
     _onLoadStart(){
-
+        console.log('start111')
     },
     //视频持续加载
     _onLoad(){
-
+        console.log('loads')
     },
     //视频进度
-    _onProgress(){
+    _onProgress(videoData){
+        console.log(videoData)
+        console.log('progress')
 
     },
     //视频播放结束
     _onEnd(){
+        console.log('end')
 
     },
     //播放错误
-    _onError(){
-
+    _onError(e){
+        console.log('error')
+        console.log(e)
     }
 });
 
@@ -87,15 +92,17 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         backgroundColor: '#F5FCFF',
     },
-    welcome: {
-        fontSize: 20,
-        textAlign: 'center',
-        margin: 10,
+
+    videoBox:{
+        width: width,
+        height: 360,
+        backgroundColor: '#000'
     },
-    instructions: {
-        textAlign: 'center',
-        color: '#333333',
-        marginBottom: 5,
+
+    video:{
+        width: width,
+        height: 360,
+        backgroundColor: '#000'
     },
 });
 
